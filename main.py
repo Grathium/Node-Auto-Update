00ecc0f832d99a699414afe54b8ad9d8e1027035
hudson-newey <grath@duck.com>
2022-10-02 23:16:39 +1000

    added support for replacing node exec

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ import re
 import os
 from os.path import exists
 
+# if false: nodejs is added to PATH via link
+# if True: nodejs is added by adding the direct executable to the PATH
+DIRECT_CALLING = False
+
 # get the most recent NodeJS version from the website
 def getNodeVersion(LTS = False):
     r = requests.get('https://nodejs.org/en/download/releases/')
@@ -22,8 +26,10 @@ def getNodeVersion(LTS = False):
         return 'Error'
 
 # install the most recent NodeJS version
-def installVersion(versionNumber):
-    currentVersion = os.popen(f"node --version").read()[1:]
+def installVersion(versionNumber, overwriteNode = False):
+    nodejsPATHExec = "node" if overwriteNode else "nodejs"
+    currentVersion = os.popen(f"{nodejsPATHExec} --version").read()[1:]
+
 
     if (currentVersion != versionNumber):
         # install nvm version
@@ -39,8 +45,11 @@ def installVersion(versionNumber):
         os.system(f"wget https://nodejs.org/dist/v{versionNumber}/node-v{versionNumber}-linux-x64.tar.gz")
         os.system(f"tar -xvzf node-v{versionNumber}-linux-x64.tar.gz")
         os.system(f"sudo mv node-v{versionNumber}-linux-x64 /opt/nodejs")
-        os.system("sudo ln -s /opt/nodejs/bin/node /usr/bin/nodejs")
-        # os.system("sudo cp /opt/nodejs/bin/node /usr/bin/nodejs")
+
+        if (DIRECT_CALLING):
+            os.system(f"sudo cp /opt/nodejs/bin/node /usr/bin/{nodejsPATHExec}")
+        else:
+            os.system(f"sudo ln -s /opt/nodejs/bin/node /usr/bin/{nodejsPATHExec}")
     else:
         print("NodeJS is currently up to date")
 
@@ -51,6 +60,11 @@ def printHelp():
     Usage: python3 ./main.py [version]
     Options for version: latest
                          [version number]
+    
+    Optional Flags:
+        --overwrite-node
+        This will overwrite your node execuatable. By default, NodeJS-Update creates a new NodeJS executable under the exec PATH name nodejs.
+        This will flag will instead replace node in your PATH. Use with caution.
     """
 
     print(helpPage)
@@ -71,7 +85,7 @@ if __name__ == "__main__":
         exit()
     
     print(f"Installing NodeJS Version {nodeVersion}")
-    installVersion(nodeVersion)
+    installVersion(nodeVersion, overwriteNode = False)
 
     print("")
     print("NodeJS installation complete")
