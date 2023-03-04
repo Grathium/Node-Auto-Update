654aec04652627118c97084fc7b82bcac946150b
hudson-newey <neweyh@qut.edu.au>
2023-03-04 22:05:37 +1000

    updated init script

--- README.md
+++ README.md
@@ -1,22 +1,25 @@
 # NodeJS Update
 
 This originally started off as a project to automatically update nodejs, but has since turned into an alternative to NVM after how much of a pain NVM was to work with.
-  
+
 ---
-  
-**Usage:**  
-`$ python3 ./nodejs_update.py (version) [Optional Flags]`  
-Options for version: {latest, version number}  
-  
-Optional CLA Flags:  
-`--overwrite-node` Overwrites `node` in path instead of using `nodejs`  
-`--force` Disables checks and forces installation. **use with caution**.  
-  
+
+## Usage
+
+`$ ./nodejs_update.sh (version) [Optional Flags]`
+Options for version: {latest, version number}
+
+### Optional CLA Flags
+
+- `--overwrite-node` Overwrites `node` in path instead of using `nodejs`
+- `--force` Disables checks and forces installation.
+
 ---
+
 **To use NodeJS executable**
-By default, NodeJS-Update creates a new executable in _PATH_ named `nodejs` which is automatically updated. To automatically update the `node` executable in _PATH_, use the `--overwrite-node` CLA flag.  
-Find nodeJS version  
-`$ nodejs --version`  
-  
-To run a file called index.js with nodejs  
+By default, NodeJS-Update creates a new executable in _PATH_ named `nodejs` which is automatically updated. To automatically update the `node` executable in _PATH_, use the `--overwrite-node` CLA flag.
+Find nodeJS version
+`$ nodejs --version`
+
+To run a file called index.js with nodejs
 `$ nodejs ./index.js`
---
+++ nodejs_update.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+# installs a local virtual environment
+initilize_python_virtual_environment() {
+    # check what python version to use
+    which python3;
+    USE_PYTHON3=0;
+    if [ $? -eq 0 ]; then
+        USE_PYTHON3=1;
+    else
+        which python;
+        if [ $? -eq 0 ]; then
+            USE_PYTHON3=0;
+        else
+            echo "Error: Neither Python2 or Python3 found in PATH";
+            exit;
+        fi
+    fi
+
+    # create the python virtual environment
+    if [ $USE_PYTHON3 -eq 1 ]; then
+        python3 -m venv ./venv/;
+    else
+        python -m venv ./venv/;
+    fi
+
+    if [ -d ./venv/bin/ ]; then
+        # user is on linux
+        source ./venv/bin/activate;
+    else
+        # user is on windows
+        source ./venv/Scripts/activate;
+    fi
+
+    # upgrade pip
+    if [ $USE_PYTHON3 -eq 1 ]; then
+        python3 -m pip install --upgrade pip;
+    else
+        python -m pip install --upgrade pip;
+    fi
+
+    # install requirements.txt
+    if [ -f ./requirements.txt ]; then
+        if [ $USE_PYTHON3 -eq 1 ]; then
+            pip3 install -r ./requirements.txt;
+        else
+            pip install -r ./requirements.txt;
+        fi
+    else
+        echo "Creating empty requirements.txt";
+        touch requirements.txt;
+    fi
+}
+
+# call activate() to initilize & install python3 virtual environment
+activate_python() {
+    if [ ! -d ./venv/ ]; then
+        initilize_python_virtual_environment;
+    else
+        # use the correct activate command whether the user is on linux or windows
+        if [ -d ./venv/bin/ ]; then
+            # user is on linux
+            source ./venv/bin/activate;
+        else
+            # user is on windows
+            source ./venv/Scripts/activate;
+        fi
+    fi
+}
+
+# call this at the end of your program to deactivate the python3 virtual environment
+deactivate_python() {
+    deactivate;
+
+    if [ -d ./venv/ ]; then
+        rm -r ./venv/;
+    fi
+}
+
+init_environment() {
+  if [ ! -f ./.gitignore ]; then
+    echo "Creating Python .gitignore";
+    wget https://raw.githubusercontent.com/github/gitignore/main/Python.gitignore -O .gitignore;
+  fi
+}
+
+main() {
+  if [ "$(id -u)" -eq 0 ]; then
+    echo -e "\e[93m[WARNING] Python is running as Root!\e[39m";
+  fi
+
+  init_environment;
+
+  activate_python;
+  python3 ./src/main.py $@;
+  deactivate_python;
+}
+
+main $@;
